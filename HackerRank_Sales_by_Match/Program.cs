8bb289f18196a8475e0adc72f0664cde27f59c73
profesordavidvaldez <42122724+profesordavidvaldez@users.noreply.github.com>
2021-06-09 17:27:28 -0400

    Challenge solved

---
+++ HackerRank_Sales_by_Match/Program.cs
@@ -0,0 +1,19 @@
+﻿using HackerRank_Sales_by_Match;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
---
+++ HackerRank_Sales_by_Match/Result.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank_Sales_by_Match
+{
+    class Result
+    {
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int totalApparences = 0;
+            int pairsCount = 0;
+
+            var numbersToFind = ar.Distinct().ToList();
+
+            foreach (var item in numbersToFind)
+            {
+                for (int i = 0; i < ar.Count(); i++)
+                {
+                    if (item.Equals(ar[i]))
+                    {
+                        totalApparences++;
+
+                        if (totalApparences % 2 == 0)
+                        {
+                            pairsCount++;
+                            totalApparences = 0;
+                        }
+                    }
+                }
+
+                totalApparences = 0;
+            }
+            return pairsCount;
+        }
+    }
+}
